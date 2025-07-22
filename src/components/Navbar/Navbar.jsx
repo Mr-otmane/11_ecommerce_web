e621996c814ec9790762b27eee90cdd5d3f9e362
Mr otmane <otmaneanna2020@gmail.com>
2025-07-22 15:19:03 +0100

    make style for new classname

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div>
       {/* upper Navbar */}
       <div>
-        <div>
+        <div className="container flex justify-between items-center">
           <div>
-            <a href="#" className="font-bold text-2xl">
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
               <img src={Logo} alt="Logo" className="w-10" />
               Shopsy
             </a>
