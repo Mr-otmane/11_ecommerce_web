c52ccc5849a320db870ec88b038ced5ee0d56675
Mr otmane <otmaneanna2020@gmail.com>
2025-07-23 23:52:45 +0100

    put new div tags

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div>
+    upper Navbar
+
+    <div></div>
+    lower Navbar
+     <div></div>  
+  </div>;
+
 };
 
 export default Navbar;
