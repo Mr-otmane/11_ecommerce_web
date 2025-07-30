073375531d4ba197f7c55e99b13ee266c4056f8e
Mr otmane <otmaneanna2020@gmail.com>
2025-07-30 22:09:26 +0100

    put ancert tags

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        upper Navbar
-        <div></div>
-        lower Navbar
+        {/* upper Navbar */}
+        <div>
+            <div>
+                <a href="#"></a>
+            </div>
+        </div>
+        {/* lower Navbar */}
         <div></div>
     </div>
   )
