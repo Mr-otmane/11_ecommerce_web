b8b1d0320b2bf228c8c1717b30ff61f1a20fd2d4
Mr otmane <otmaneanna2020@gmail.com>
2025-07-31 23:26:49 +0100

    make styling for input tags

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import Logo from "../../assets/logo.png"
+import React from "react";
+import Logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
     <div>
-        {/* upper Navbar */}
+      {/* upper Navbar */}
+      <div>
         <div>
-            <div>
-                <div>
-                <a href="#">
-                    <img src={Logo} alt="Logo" />
-                </a>
-
+          <div>
+            <a href="#">
+              <img src={Logo} alt="Logo" />
+            </a>
+          </div>
+          {/* search bar and order button */}
+          <div>
+            <div className="group">
+              <input
+                type="text"
+                placeholder="search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition all duration-300 rouded-full border border-gray-300"
+              />
             </div>
-            {/* search bar and order button */}
-            <div>
-                <div className='group'>
-                    <input type="text" placeholder='search'
-                    className='w-[200px] sm:w-[200px] group-hover:w-[300px]' />
-                </div>
-            </div>
+          </div>
         </div>
-        </div>
-        {/* lower Navbar */}
-        <div></div>
+      </div>
+      {/* lower Navbar */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
