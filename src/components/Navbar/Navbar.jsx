13501b98b46b3d33607e770f8de1a012aa156e57
Mr otmane <otmaneanna2020@gmail.com>
2025-08-01 23:44:31 +0100

    making styling for this classname

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div>
         <div>
           <div>
-            <a href="#" className="font-bold">
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
               <img src={Logo} alt="Logo" className="w-10 " />
               Shopsy
             </a>
